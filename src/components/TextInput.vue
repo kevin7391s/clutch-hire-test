9d97b7454b5eae9533b460da7819eed2146bac8a
Kevin7391s <54608398+kevin7391s@users.noreply.github.com>
2025-04-21 23:01:53 -0400

    add reusable TextInput componen
    - created a component that accepts label, name, type, and type as props
    - component is built to work with v-model to send and receive values from parent element

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>TextInput</div>
+  <div class="text-input">
+    <label :for="name">{{ label }}</label>
+    <!-- Input field inherits its id, name, type, and placeholder from props -->
+    <input
+      :id="name"
+      :name="name"
+      :type="type"
+      :value="modelValue"
+      @input="
+        // Emits the user value as user types, targeted as HTMLInputElement
+        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+      "
+    />
+  </div>
 </template>
 
 <script lang="ts">
 export default {
   name: "TextInput",
+  // Props define input behavior and allow parent components to control content
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+
+    type: {
+      type: String,
+      default: "text",
+    },
+    modelValue: {
+      type: String,
+      default: "",
+    },
+  },
+  emits: ["update:modelValue"],
 };
 </script>
